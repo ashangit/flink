b5bb1fb267a78e4e20949bfb3ba0804fd68f264b
nicolas.fraison@datadoghq.com <nicolas.fraison@datadoghq.com>
2025-02-28 10:25:20 +0100

    Add license

--- flink-rpc/flink-rpc-akka/src/main/java/org/apache/pekko/remote/transport/netty/SSLContextLoader.java
+++ flink-rpc/flink-rpc-akka/src/main/java/org/apache/pekko/remote/transport/netty/SSLContextLoader.java
@@ -1,4 +1,32 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.apache.pekko.remote.transport.netty;
+
+import org.apache.flink.shaded.netty4.io.netty.handler.ssl.util.FingerprintTrustManagerFactory;
+
+import org.apache.pekko.remote.RemoteTransportException;
+
+import javax.net.ssl.KeyManager;
+import javax.net.ssl.KeyManagerFactory;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.TrustManagerFactory;
 
 import java.io.IOException;
 import java.io.InputStream;
@@ -11,18 +39,8 @@ import java.security.KeyStoreException;
 import java.security.NoSuchAlgorithmException;
 import java.security.SecureRandom;
 import java.security.UnrecoverableKeyException;
-
 import java.util.List;
 
-import javax.net.ssl.KeyManager;
-import javax.net.ssl.KeyManagerFactory;
-import javax.net.ssl.SSLContext;
-import javax.net.ssl.TrustManager;
-import javax.net.ssl.TrustManagerFactory;
-
-import org.apache.flink.shaded.netty4.io.netty.handler.ssl.util.FingerprintTrustManagerFactory;
-
-import org.apache.pekko.remote.RemoteTransportException;
 
 public class SSLContextLoader {
 
@@ -34,8 +52,13 @@ public class SSLContextLoader {
     private final String sslTrustStoreType;
     private SSLContext sslContext;
 
-
-    public SSLContextLoader(SSLSettings ssLSettings, String sslTrustStore, String sslTrustStorePassword, String sslKeyStoreType, String sslTrustStoreType,  List<String> sslCertFingerprints) {
+    public SSLContextLoader(
+            SSLSettings ssLSettings,
+            String sslTrustStore,
+            String sslTrustStorePassword,
+            String sslKeyStoreType,
+            String sslTrustStoreType,
+            List<String> sslCertFingerprints) {
         this.ssLSettings = ssLSettings;
         this.sslCertFingerprints = sslCertFingerprints;
         this.sslKeyStoreType = sslKeyStoreType;
